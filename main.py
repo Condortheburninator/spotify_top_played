032543c288cdb11f4885b074cc29666b54ac8c78
Condortheburninator <54361500+Condortheburninator@users.noreply.github.com>
2022-03-06 13:22:35 -0700

    tided up formatting

--- main.py
+++ main.py
@@ -49,10 +49,10 @@ if __name__ == "__main__":
 
         # Prepare a dictionary in order to turn it into a pandas dataframe below
     song_dict = {
-        "song_name" : song_names,
-        "artist_name": artist_names,
-        "played_at" : played_at_list,
-        "timestamp" : timestamps
+         "song_name"     : song_names
+        ,"artist_name"   : artist_names
+        ,"played_at"     : played_at_list
+        ,"timestamp"     : timestamps
     }
 
     song_df = pd.DataFrame(song_dict, columns = ["song_name", "artist_name", "played_at", "timestamp"])
